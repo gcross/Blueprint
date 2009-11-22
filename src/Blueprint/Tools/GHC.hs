ff76bbde3b4c3ef0d69e361d1e5282943577d84d
Gregory Crosswhite <gcross@phys.washington.edu>
2009-11-21 23:37:29 -0800

    Added dependency scanner.

--- src/Blueprint/Tools/GHC.hs
+++ src/Blueprint/Tools/GHC.hs
@@ -6,9 +6,17 @@ module Blueprint.Tools.GHC where
 
 -- @<< Import needed modules >>
 -- @+node:gcross.20091121210308.1269:<< Import needed modules >>
+import Data.Array
+import qualified Data.ByteString.Lazy as L
+import qualified Data.ByteString.Lazy.Char8 as L8
+import Data.Either.Unwrap
+
 import System.Directory
 import System.IO.Unsafe
 import System.Process
+
+import Text.Regex.TDFA
+import Text.Regex.TDFA.ByteString.Lazy
 
 import Blueprint.Miscellaneous
 -- @-node:gcross.20091121210308.1269:<< Import needed modules >>
@@ -26,6 +34,24 @@ data GHCTools = GHCTools
 data GHCCompilerOptions = GHCCompilerOptions ()
 -- @-node:gcross.20091121210308.1272:GHCCompilerOptions
 -- @-node:gcross.20091121210308.1270:Types
+-- @+node:gcross.20091121210308.2014:Values
+-- @+node:gcross.20091121210308.2015:regular expressions
+import_matching_regex = fromRight . compile defaultCompOpt defaultExecOpt . L8.pack $ "\\s*import +(qualified +)?([A-Z][A-Za-z0-9_.]+)[\\s;]?"
+-- @-node:gcross.20091121210308.2015:regular expressions
+-- @-node:gcross.20091121210308.2014:Values
+-- @+node:gcross.20091121210308.2016:Functions
+-- @+node:gcross.20091121210308.2017:dependenciesOf
+dependenciesOf :: FilePath -> [String]
+dependenciesOf =
+    map (L8.unpack . fst . (! 2))
+    .
+    matchAllText import_matching_regex
+    .
+    unsafePerformIO
+    .
+    L.readFile
+-- @-node:gcross.20091121210308.2017:dependenciesOf
+-- @-node:gcross.20091121210308.2016:Functions
 -- @+node:gcross.20091121210308.1273:Configuration
 -- @+node:gcross.20091121210308.1274:ghcTools
 ghcTools :: Maybe GHCTools
