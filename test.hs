deca539b5725d503eb9c4f1feefb2459c2a81c8e
Gregory Crosswhite <gcross@phys.washington.edu>
2010-06-03 14:44:35 -0700

    Added some tests of findConflicts.

--- test.hs
+++ test.hs
@@ -10,7 +10,12 @@
 
 -- @<< Import needed modules >>
 -- @+node:gcross.20100602152546.1869:<< Import needed modules >>
+import Control.Arrow
+
+import Data.Map (Map)
 import qualified Data.Map as Map
+import Data.Set (Set)
+import qualified Data.Set as Set
 
 import Test.HUnit
 import Test.Framework
@@ -48,8 +53,121 @@ import Blueprint.Options
 main = defaultMain
     -- @    << Tests >>
     -- @+node:gcross.20100602152546.1870:<< Tests >>
-    [
     -- @+others
+    -- @+node:gcross.20100603132252.1293:findConflicts
+    [testGroup "findConflicts"
+        -- @    @+others
+        -- @+node:gcross.20100603132252.1300:lists with () values
+        [testGroup "lists with () values"
+            -- @    @+others
+            -- @+node:gcross.20100603132252.1294:empty list
+            [testCase "empty list" $
+                assert . Map.null . findConflicts (undefined :: Set ()) undefined undefined $ ([] :: [()])
+            -- @-node:gcross.20100603132252.1294:empty list
+            -- @+node:gcross.20100603132252.1296:singleton list
+            ,testCase "singleton list" $
+                assertEqual
+                    "Is the output set correct?"
+                    Map.empty
+                    .
+                    findConflicts Set.empty (const "") Set.singleton
+                    $
+                    [()]
+            -- @-node:gcross.20100603132252.1296:singleton list
+            -- @+node:gcross.20100603132252.1298:2-element list with conflicts
+            ,testCase "2-element list with conflicts" $
+                assertEqual
+                    "Is the output set correct?"
+                    (Map.singleton () (Set.fromList ["A","B"]))
+                    .
+                    findConflicts Set.empty fst (Set.singleton . snd)
+                    $
+                    [("A",())
+                    ,("B",())
+                    ]
+            -- @-node:gcross.20100603132252.1298:2-element list with conflicts
+            -- @+node:gcross.20100603132252.1299:list with simple resolved conflicts
+            ,testCase "list with simple resolved conflicts" $
+                assertEqual
+                    "Is the output set correct?"
+                    Map.empty
+                    .
+                    findConflicts (Set.singleton ()) fst (Set.singleton . snd)
+                    $
+                    [("A",())
+                    ,("B",())
+                    ,("C",())
+                    ]
+            -- @-node:gcross.20100603132252.1299:list with simple resolved conflicts
+            -- @-others
+            ]
+        -- @-node:gcross.20100603132252.1300:lists with () values
+        -- @+node:gcross.20100603132252.1306:lists with Int values
+        ,testGroup "lists with Int values"
+            -- @    @+others
+            -- @+node:gcross.20100603132252.1309:list with no conflicts
+            [testCase "list with no conflicts" $
+                assertEqual
+                    "Is the output set correct?"
+                    Map.empty
+                    .
+                    findConflicts Set.empty fst (Set.fromList . snd)
+                    $
+                    [("A",[1,2])
+                    ,("B",[3,4])
+                    ,("C",[5,6,7,8])
+                    ]
+            -- @-node:gcross.20100603132252.1309:list with no conflicts
+            -- @+node:gcross.20100603132252.1310:list with unresolved conflicts
+            ,testCase "list with unresolved conflicts" $
+                assertEqual
+                    "Is the output set correct?"
+                    (
+                        Map.fromList
+                        .
+                        map (second Set.fromList)
+                        $
+                        [(2,["A","B","D"])
+                        ,(3,["B","D"])
+                        ]
+                    )
+                    .
+                    findConflicts Set.empty fst (Set.fromList . snd)
+                    $
+                    [("A",[1,2])
+                    ,("B",[2,3])
+                    ,("C",[5,6,7,8])
+                    ,("D",[2,3,4])
+                    ]
+            -- @-node:gcross.20100603132252.1310:list with unresolved conflicts
+            -- @+node:gcross.20100603132252.1312:list with some resolved conflicts
+            ,testCase "list with some resolved conflicts" $
+                assertEqual
+                    "Is the output set correct?"
+                    (
+                        Map.fromList
+                        .
+                        map (second Set.fromList)
+                        $
+                        [(3,["B","D"])
+                        ]
+                    )
+                    .
+                    findConflicts (Set.fromList [1,2]) fst (Set.fromList . snd)
+                    $
+                    [("A",[1,2])
+                    ,("B",[2,3])
+                    ,("C",[5,6,7,8])
+                    ,("D",[2,3,4])
+                    ,("E",[1])
+                    ]
+            -- @-node:gcross.20100603132252.1312:list with some resolved conflicts
+            -- @-others
+            ]
+        -- @-node:gcross.20100603132252.1306:lists with Int values
+        -- @-others
+        ]
+    -- @-node:gcross.20100603132252.1293:findConflicts
     -- @-others
     -- @-node:gcross.20100602152546.1870:<< Tests >>
     -- @nl
