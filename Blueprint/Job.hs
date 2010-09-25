1f1b2ac4a1fee6e9d48f24322b9de72da7ae832c
gcross <gcross@phys.washington.edu>
2010-09-25 10:58:22 -0700

    Implemented runJob.

--- Blueprint/Job.hs
+++ Blueprint/Job.hs
@@ -4,6 +4,8 @@
 -- @<< Language extensions >>
 -- @+node:gcross.20100924160650.2045:<< Language extensions >>
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE Rank2Types #-}
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE UnicodeSyntax #-}
 -- @-node:gcross.20100924160650.2045:<< Language extensions >>
 -- @nl
@@ -13,12 +15,20 @@ module Blueprint.Job where
 -- @<< Import needed modules >>
 -- @+node:gcross.20100924160650.2046:<< Import needed modules >>
 import Control.Applicative
+import Control.Concurrent
+import Control.Concurrent.Chan
 import Control.Exception
 import Control.Monad
 
 import Data.Binary
+import qualified Data.ByteString.Lazy as L
+import Data.Dynamic
+import Data.IVar.Simple (IVar)
+import qualified Data.IVar.Simple as IVar
+import Data.IORef
 import Data.Map (Map)
 import qualified Data.Map as Map
+import Data.Maybe
 import Data.Typeable
 import Data.UUID
 -- @-node:gcross.20100924160650.2046:<< Import needed modules >>
@@ -35,6 +45,24 @@ data Job α where
     Once :: Typeable α ⇒ UUID → Job α → (α → Job β) → Job β
     Cache :: Binary α ⇒ UUID → α → (α → Job (α,β)) → (β → Job ɣ) → Job ɣ
 -- @-node:gcross.20100924160650.2048:Job
+-- @+node:gcross.20100925004153.1301:JobEnvironment
+data JobEnvironment = JobEnvironment
+    {   environmentCompletedJobs :: IORef (Map UUID Dynamic)
+    ,   environmentTaskRunner :: (∀ α. IO α → IO α)
+    ,   environmentInputCache :: Map UUID L.ByteString
+    ,   environmentOutputCache :: Chan (UUID,L.ByteString)
+    }
+-- @-node:gcross.20100925004153.1301:JobEnvironment
+-- @+node:gcross.20100925004153.1302:TaskRunner
+data TaskRunner = TaskRunner
+    {   taskRunnerThreads :: [ThreadId]
+    ,   taskRunnerQueue :: Chan TaskSubmission
+    }
+-- @-node:gcross.20100925004153.1302:TaskRunner
+-- @+node:gcross.20100925004153.1303:TaskSubmission
+data TaskSubmission where
+    TaskSubmission :: IO α → IVar α → TaskSubmission
+-- @-node:gcross.20100925004153.1303:TaskSubmission
 -- @-node:gcross.20100924160650.2047:Types
 -- @+node:gcross.20100924174906.1276:Instances
 -- @+node:gcross.20100924174906.1278:Applicative Job
@@ -64,6 +92,108 @@ instance Monad Job where
     Cache uuid x f g >>= h = Cache uuid x f (g >=> h)
 -- @-node:gcross.20100924174906.1277:Monad Job
 -- @-node:gcross.20100924174906.1276:Instances
+-- @+node:gcross.20100925004153.1297:Functions
+-- @+node:gcross.20100925004153.1299:cache
+cache :: Binary α ⇒ UUID → α → (α → Job (α,β)) → Job β
+cache uuid starting_value computeJob = Cache uuid starting_value computeJob return
+-- @-node:gcross.20100925004153.1299:cache
+-- @+node:gcross.20100925004153.1298:once
+once :: Typeable α ⇒ UUID → Job α → Job α
+once uuid job = Once uuid job return
+-- @-node:gcross.20100925004153.1298:once
+-- @+node:gcross.20100925004153.1307:runJob
+runJob :: JobEnvironment → Job α → IO (Either (Map String SomeException) α)
+runJob job_environment@JobEnvironment{..} job =
+    case job of
+        Result x → return (Right x)
+        Task io computeNextJob →
+            environmentTaskRunner io
+            >>=
+            runJob job_environment . computeNextJob
+        Fork jf jx computeNextJob → do
+            f_ivar ← IVar.new
+            forkIO $ nestedRunJob jf >>= IVar.write f_ivar
+            x_ivar ← IVar.new
+            forkIO $ nestedRunJob jx >>= IVar.write x_ivar
+            case (IVar.read f_ivar, IVar.read x_ivar) of
+                (Right f, Right x) → nestedRunJob (computeNextJob (f x))
+                (Left errors, Right _) → return (Left errors)
+                (Right _, Left errors) → return (Left errors)
+                (Left errors1, Left errors2) → return (Left (Map.union errors1 errors2))
+        Errors errors → return (Left errors)
+        Once uuid job computeNextJob → do
+            result_ivar ← IVar.new
+            maybe_previous_result ←
+                atomicModifyIORef environmentCompletedJobs $ \completed_jobs →
+                    case Map.lookup uuid completed_jobs of
+                        Nothing →
+                            (Map.insert uuid (IVar.read result_ivar) completed_jobs
+                            ,Nothing
+                            )
+                        Just previous_result →
+                            (completed_jobs
+                            ,Just previous_result
+                            )
+            case maybe_previous_result of
+                Nothing → do
+                    job_result ← nestedRunJob job
+                    case job_result of
+                        Left errors → return (Left errors)
+                        Right result → do
+                            IVar.write result_ivar (toDyn result)
+                            nestedRunJob (computeNextJob result)
+                Just previous_result →
+                    nestedRunJob
+                    .
+                    computeNextJob
+                    .
+                    fromJust
+                    .
+                    fromDynamic
+                    $
+                    previous_result
+        Cache uuid starting_value computeJob computeNextJob → do
+            let old_cached_value =
+                    fromMaybe starting_value
+                    .
+                    fmap decode
+                    .
+                    Map.lookup uuid
+                    $
+                    environmentInputCache
+            job_result ← nestedRunJob (computeJob old_cached_value)
+            case job_result of
+                Left errors → return (Left errors)
+                Right (new_cached_value,job_result) → do
+                    writeChan environmentOutputCache (uuid,encode new_cached_value)
+                    nestedRunJob (computeNextJob job_result)
+  where
+    nestedRunJob = runJob job_environment
+-- @-node:gcross.20100925004153.1307:runJob
+-- @+node:gcross.20100925004153.1306:startTaskRunner
+startTaskRunner :: Int → IO TaskRunner
+startTaskRunner number_of_slaves = do
+    task_queue ← newChan
+    slave_thread_ids ←
+        replicateM number_of_slaves $
+            startTaskSlave task_queue
+    return $
+        TaskRunner
+            slave_thread_ids
+            task_queue
+-- @-node:gcross.20100925004153.1306:startTaskRunner
+-- @+node:gcross.20100925004153.1304:startTaskSlave
+startTaskSlave :: Chan TaskSubmission → IO ThreadId
+startTaskSlave task_queue =
+    forkIO . forever $
+        readChan task_queue
+        >>=
+        \(TaskSubmission task ivar) →
+            fmap (either (throw :: SomeException → α) id) (try task)
+            >>=
+            IVar.write ivar
+-- @-node:gcross.20100925004153.1304:startTaskSlave
+-- @-node:gcross.20100925004153.1297:Functions
 -- @-others
 -- @-node:gcross.20100924160650.2044:@thin Job.hs
 -- @-leo
