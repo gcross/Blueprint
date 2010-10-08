6d8648c414824496ec1771e484ae9047f3b16083
Gregory Crosswhite <gcross@phys.washington.edu>
2010-10-07 19:00:37 -0700

    Added main options.

--- Blueprint/Main.hs
+++ Blueprint/Main.hs
@@ -13,11 +13,13 @@ module Blueprint.Main where
 -- @+node:gcross.20101007134409.1497:<< Import needed modules >>
 import Data.Map (Map)
 import qualified Data.Map as Map
+import Data.Monoid
 
 import System.Environment
 import System.Exit
 import System.Log.Logger
 
+import Blueprint.Identifier
 import Blueprint.Job
 import Blueprint.Options
 -- @nonl
@@ -36,7 +38,7 @@ defaultMain ::
     Map String (α → Job ()) →
     IO ()
 defaultMain
-    options
+    user_options
     configuration_filepath
     configuration_cache_filepath
     cache_filepath
@@ -53,8 +55,10 @@ defaultMain
                     options
                     configuration_filepath
                     command_line_option_values
-            runJobUsingCacheFileAndExtractResult 1 configuration_cache_filepath $
-                configure option_values
+            runJobUsingCacheFileAndExtractResult
+                (extractNumberOfSimultaneousTasksFrom option_values)
+                configuration_cache_filepath
+                (configure option_values)
             return ()
         else do
             job ← case Map.lookup mode modes of
@@ -70,19 +74,52 @@ defaultMain
                 getAndParseConfigurationFile
                     options
                     configuration_filepath
+            let number_of_simultaneous_tasks = extractNumberOfSimultaneousTasksFrom option_values
             (runJobUsingCacheFileAndExtractResult
-                1
+                number_of_simultaneous_tasks
                 configuration_cache_filepath
                 (configure option_values)
              >>=
              runJobUsingCacheFileAndExtractResult
-                1
+                number_of_simultaneous_tasks
                 cache_filepath
              .
              job
              )
+  where
+    options = user_options `mappend` main_options
+
+    extractNumberOfSimultaneousTasksFrom :: OptionValues → Int
+    extractNumberOfSimultaneousTasksFrom =
+        maybe 1 read
+        .
+        Map.lookup main_option_number_of_simultaneous_tasks
 -- @-node:gcross.20101007134409.1500:defaultMain
 -- @-node:gcross.20101007134409.1499:Functions
+-- @+node:gcross.20101007134409.1504:Options
+main_option_number_of_simultaneous_tasks = identifier "4b286db3-9976-472d-a865-2e2e7e5cb2aa" "number of simultaneous tasks"
+main_option_help = identifier "027b2e80-afc4-4429-85f7-0752eff276ec" "help"
+
+main_options =
+    Options
+        (Map.fromList
+            [('j',(main_option_number_of_simultaneous_tasks,RequiredArgument "#"))
+            ,('h',(main_option_help,NoArgument "1")) -- '
+            ]
+        )
+        (Map.fromList
+            [("number-of-simultaneous-tasks",(main_option_number_of_simultaneous_tasks,RequiredArgument "#"))
+            ,("help",(main_option_help,NoArgument "1"))
+            ]
+        )
+        Map.empty
+        Map.empty
+        (Map.fromList
+            [(main_option_number_of_simultaneous_tasks,("Main","Number of tasks to run simultaneously (defaults to 1)"))
+            ,(main_option_help,("Main","Display this help message and then exit."))
+            ]
+        )
+-- @-node:gcross.20101007134409.1504:Options
 -- @-others
 -- @-node:gcross.20101007134409.1495:@thin Main.hs
 -- @-leo
